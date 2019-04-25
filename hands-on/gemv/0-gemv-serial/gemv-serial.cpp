db5897cbfa102fc99e2194751afe01e2cfd8173f
Ye Luo <yeluo@anl.gov>
2019-04-25 00:14:50 -0500

    Update files.

--- hands-on/gemv/0-gemv-serial/gemv-serial.cpp
+++ hands-on/gemv/0-gemv-serial/gemv-serial.cpp
@@ -1,5 +1,7 @@
 #define N 8192
 #include <iostream>
+#include <chrono>
+#include <string>
 
 template <typename T>
 void gemv(int n, T alpha, const T* restrict A, const T* restrict V, T* restrict Vout)
@@ -27,13 +29,32 @@ void deallocate(T* ptr, int n)
   delete[] ptr;
 }
 
+class Timer
+{
+  const std::chrono::time_point<std::chrono::system_clock> start;
+  const std::string name;
+
+public:
+  Timer(const std::string& name_in): start(std::chrono::system_clock::now()), name(name_in) {};
+  ~Timer()
+  {
+    auto end = std::chrono::system_clock::now();
+    std::cout << "Function " << name
+              << " takes " << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count()
+              << " us" << std::endl;
+  }
+};
+
 int main()
 {
   auto* A = allocate<float>(N*N);
   auto* V = allocate<float>(N);
   auto* Vout = allocate<float>(N);
 
-  gemv(N, 1.0f, A, V, Vout);
+  {
+    Timer local("GEMV");
+    gemv(N, 1.0f, A, V, Vout);
+  }
 
   deallocate(A, N*N);
   deallocate(V, N);
