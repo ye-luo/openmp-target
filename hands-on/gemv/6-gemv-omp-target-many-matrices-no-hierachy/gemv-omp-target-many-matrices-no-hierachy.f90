0ae24c384b3a902118b24b54b506df6b256ed264
Pankaj Rajak <prajak@jlselogin2.ftm.alcf.anl.gov>
2019-09-11 18:21:51 +0000

    updated openmp fortran code

--- hands-on/gemv/6-gemv-omp-target-many-matrices-no-hierachy/gemv-omp-target-many-matrices-no-hierachy.f90
+++ hands-on/gemv/6-gemv-omp-target-many-matrices-no-hierachy/gemv-omp-target-many-matrices-no-hierachy.f90
@@ -21,16 +21,17 @@ if(err/=0) print'(a30,i9,i3)', 'ERROR in allocation for V',err
 allocate(Vout(1:Num_calc,1:N),stat=err)
 if(err/=0) print'(a30,i9,i3)', 'ERROR in allocation for Vout',err
   
-A(:,:) = 1.0
-V(:,:) = 1.0
 
 !$omp parallel do
 do i=1,Num_calc
+   A(i,:) = 1.0
+   V(i,:) = 1.0
    call gemv(N,alpha,A(i,:),V(i,:),Vout(i,:))
 end do
 !$omp end parallel do
 
 do i=1,Num_calc
+!$omp target update from(Vout(i,:))
    do val=1,N
      if (int(Vout(i,val)) .NE. N) then
         write(*,*) "Value does not match at",i,val,int(Vout(i,val)) 
@@ -62,14 +63,14 @@ real(8) :: alpha,sum_val
 real(8),intent(in) :: A(1:nval*nval),V(1:nval)
 real(8),intent(inout):: Vout(1:nval)
 
+!$omp target teams distribute map(to:A,V) map(from:Vout)
 do row=1,nval
-   tid=OMP_GET_THREAD_NUM()
    sum_val = 0.0
    A_row =(row-1)*nval
-   write(*,*) "total number of threads: ",tid,A_row,A_row+nval,nval*nval
    do col=1,nval
       sum_val = sum_val + A(A_row+nval)*V(col)
    end do
    Vout(row) = sum_val * alpha
 end do
+!$omp end target teams distribute
 end subroutine
--- hands-on/gemv/7-gemv-omp-target-many-matrices/gemv-omp-target-many-matrices.f90
+++ hands-on/gemv/7-gemv-omp-target-many-matrices/gemv-omp-target-many-matrices.f90
@@ -4,28 +4,39 @@ USE OMP_LIB
 
 implicit none
 integer,parameter:: N=64
-real(8),allocatable :: A(:),V(:),Vout(:)
+real(8),allocatable :: A(:,:),V(:,:),Vout(:,:)
 real(8) :: alpha=1.0
 integer :: ti,tj,tk
 integer :: err
-integer :: val
+integer :: i,val
+integer,parameter :: Num_calc=8
 
 !!starts here
 call system_clock(ti,tk)
-allocate(A(1:N*N),stat=err)
-if(err/=0) print'(a30,i9,i3)', 'ERROR in allocation for A',err
-allocate(V(1:N),stat=err)
-if(err/=0) print'(a30,i9,i3)', 'ERROR in allocation for V',err
-allocate(Vout(1:N),stat=err)
-if(err/=0) print'(a30,i9,i3)', 'ERROR in allocation for Vout',err
 
-A(:) = 1.0
-V(:) = 1.0
-call gemv(N,alpha,A,V,Vout)
-do val=1,N
-   if (int(Vout(val)) .NE. N) then
-        write(*,*) "Value does not match at",val,int(Vout(val)) 
-   end if
+allocate(A(1:Num_calc,1:N*N),stat=err)
+if(err/=0) print'(a30,i9,i3)', 'ERROR in allocation for A',err
+allocate(V(1:Num_calc,1:N),stat=err)
+if(err/=0) print'(a30,i9,i3)', 'ERROR in allocation for V',err
+allocate(Vout(1:Num_calc,1:N),stat=err)
+if(err/=0) print'(a30,i9,i3)', 'ERROR in allocation for Vout',err
+  
+
+!$omp parallel do
+do i=1,Num_calc
+   A(i,:) = 1.0
+   V(i,:) = 1.0
+   call gemv(N,alpha,A(i,:),V(i,:),Vout(i,:))
+end do
+!$omp end parallel do
+
+do i=1,Num_calc
+!$omp target update from(Vout(i,:))
+   do val=1,N
+     if (int(Vout(i,val)) .NE. N) then
+        write(*,*) "Value does not match at",i,val,int(Vout(i,val)) 
+     end if
+   end do
 end do
 
 
@@ -52,18 +63,15 @@ real(8) :: alpha,sum_val
 real(8),intent(in) :: A(1:nval*nval),V(1:nval)
 real(8),intent(inout):: Vout(1:nval)
 
-!$omp target  data map(to:A,V) map(from:Vout) 
-!$omp parallel do default(shared),private(tid,row,col,A_row,sum_val)
+!$omp target teams distribute map(to:A,V) map(from:Vout)
 do row=1,nval
-   tid=OMP_GET_THREAD_NUM()
    sum_val = 0.0
    A_row =(row-1)*nval
-   write(*,*) "total number of threads: ",tid,A_row,A_row+nval,nval*nval
+!$omp parallel do default(shared),private(A_row),reduction(+:sum_val)
    do col=1,nval
       sum_val = sum_val + A(A_row+nval)*V(col)
    end do
    Vout(row) = sum_val * alpha
 end do
-!$omp end parallel do
-!$omp end target data
+!$omp end target teams distribute
 end subroutine
