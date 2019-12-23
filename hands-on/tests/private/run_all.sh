d2625ba99ad8072927f03aa72acd0db902877570
Ye Luo <yeluo@anl.gov>
2019-12-22 23:26:50 -0600

    Update script.

--- hands-on/tests/private/run_all.sh
+++ hands-on/tests/private/run_all.sh
@@ -1,8 +1,13 @@
 CC=gcc
 CC_FLAGS="-O3 -fopenmp -foffload=nvptx-none"
 
+#CC=clang
+#CC_FLAGS="-O3 -fopenmp -fopenmp-targets=nvptx64"
+
 for name in *.c
 do
+  echo Testing $name
   $CC $CC_FLAGS $name
   ./a.out
+  echo
 done
