0a565dc8a0f66df9705640cde931d0838588ef23
Ye Luo <yeluo@anl.gov>
2020-06-09 08:07:52 -0500

    Add complex_reduction_cpu

---
+++ hands-on/tests/complex/complex_reduction_cpu.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <complex>
+
+#if !defined(__NO_UDR)
+#pragma omp declare reduction(+: std::complex<float>: omp_out += omp_in)
+#pragma omp declare reduction(+: std::complex<double>: omp_out += omp_in)
+#endif
+
+template<typename T>
+void test_reduction()
+{
+  T sum(0), sum_host(0);
+  const int size = 100;
+  T array[size];
+  for (int i = 0; i < size; i++)
+  {
+    array[i] = T(i);
+    sum_host += array[i];
+  }
+
+  #pragma omp parallel for reduction(+: sum)
+  for (int i = 0; i < size; i++)
+    sum += array[i];
+
+  if (std::abs(sum - sum_host) > 1e-6)
+    std::cout << "wrong reduction value check" << sum << " correct value " << sum_host << std::endl;
+}
+
+int main()
+{
+  test_reduction<float>();
+  test_reduction<std::complex<float>>();
+  test_reduction<double>();
+  test_reduction<std::complex<double>>();
+  return 0;
+}
