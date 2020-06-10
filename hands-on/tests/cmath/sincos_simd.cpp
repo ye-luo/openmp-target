77ebe691d25401724a2db6ea6269524bf4cf053d
Ye Luo <yeluo@anl.gov>
2020-06-10 17:11:05 -0500

    Add printing in sincos_simd

--- hands-on/tests/cmath/sincos_simd.cpp
+++ hands-on/tests/cmath/sincos_simd.cpp
@@ -1,7 +1,8 @@
 #include <cmath>
 #include <cassert>
-//#include <iostream>
-//#include <iomanip>
+#include <iostream>
+#include <cstdio>
+#include <iomanip>
 
 constexpr size_t N = 128;
 
@@ -20,6 +21,15 @@ int main()
     //std::cout << std::setprecision(14) << sinval[i] << " " << cosval[i] << std::endl;
   } 
 
+  std::cout << std::setprecision(14);
+  std::cout << "sinval[0] " << sinval[0] << " ref " << 0 << std::endl;
+  std::cout << "sinval[1] " << sinval[1] << " ref " << 0.099833416646828 << std::endl;
+  std::cout << "sinval[2] " << sinval[2] << " ref " << 0.19866933079506 << std::endl;
+  std::cout << "sinval[3] " << sinval[3] << " ref " << 0.29552020666134 << std::endl;
+  std::cout << "cosval[0] " << cosval[0] << " ref " << 1 << std::endl;
+  std::cout << "cosval[1] " << cosval[1] << " ref " << 0.99500416527803 << std::endl;
+  std::cout << "cosval[2] " << cosval[2] << " ref " << 0.98006657784124 << std::endl;
+  std::cout << "cosval[3] " << cosval[3] << " ref " << 0.95533648912561 << std::endl;
   assert( std::fabs(sinval[0]) < 1e-6);
   assert( std::fabs(sinval[1] - 0.099833416646828) < 1e-6);
   assert( std::fabs(sinval[2] - 0.19866933079506) < 1e-6);
