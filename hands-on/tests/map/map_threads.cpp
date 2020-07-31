cb18ac24040a732ebd8711f872b550584ee68f25
Ye Luo <yeluo@anl.gov>
2020-07-31 11:00:21 -0500

    Add multi thread map test case.

---
+++ hands-on/tests/map/map_threads.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+#include <cstdio>
+
+int main()
+{
+  const int N = 200000000;
+  std::vector<float> vec(N, 1.1);
+  float* vec_ptr = vec.data();
+  for (int it = 0; it < 3; it++)
+  {
+    printf("\niteration %d\n", it);
+    #pragma omp parallel for
+    for (int i = 0; i < 4; i++)
+    {
+      #pragma omp target map(to:vec_ptr[:vec.size()])
+      {
+        printf("tid %d value = %f\n", i, vec_ptr[N - 1 - i]);
+      }
+    }
+  }
+}
