22185ac853a43a39448400b64da5c50e5e7fd69c
Ye Luo <yeluo@anl.gov>
2020-08-13 22:45:16 -0500

    Print testing names.

--- hands-on/tests/complex/complex_reduction.cpp
+++ hands-on/tests/complex/complex_reduction.cpp
@@ -5,7 +5,7 @@ template<typename T>
 void test_map()
 {
   std::complex<T> a(0.2, 1), a_check;
-  #pragma omp target map(from:a_check)
+#pragma omp target map(from : a_check)
   {
     a_check = a;
   }
@@ -17,8 +17,8 @@ void test_map()
 }
 
 #if !defined(__NO_UDR)
-#pragma omp declare reduction(+: std::complex<float>: omp_out += omp_in)
-#pragma omp declare reduction(+: std::complex<double>: omp_out += omp_in)
+#pragma omp declare reduction(+ : std::complex <float> : omp_out += omp_in)
+#pragma omp declare reduction(+ : std::complex <double> : omp_out += omp_in)
 #endif
 
 template<typename T>
@@ -33,7 +33,7 @@ void test_reduction()
     sum_host += array[i];
   }
 
-  #pragma omp target teams distribute parallel for map(to: array[:size]) reduction(+: sum)
+#pragma omp target teams distribute parallel for map(to : array[:size]) reduction(+ : sum)
   for (int i = 0; i < size; i++)
     sum += array[i];
 
@@ -42,13 +42,13 @@ void test_reduction()
 
   const int nblock(10), block_size(10);
   std::complex<T> block_sum[nblock];
-  #pragma omp target teams distribute map(to: array[:size]) map(from: block_sum[:nblock])
+#pragma omp target teams distribute map(to : array[:size]) map(from : block_sum[:nblock])
   for (int ib = 0; ib < nblock; ib++)
   {
     std::complex<T> partial_sum;
     const int istart = ib * block_size;
-    const int iend = (ib + 1) * block_size;
-    #pragma omp parallel for reduction(+: partial_sum)
+    const int iend   = (ib + 1) * block_size;
+#pragma omp parallel for reduction(+ : partial_sum)
     for (int i = istart; i < iend; i++)
       partial_sum += array[i];
     block_sum[ib] = partial_sum;
@@ -58,7 +58,8 @@ void test_reduction()
   for (int ib = 0; ib < nblock; ib++)
     sum += block_sum[ib];
   if (std::abs(sum - sum_host) > 1e-6)
-    std::cout << "hierarchical parallelism wrong reduction value check" << sum << " correct value " << sum_host << std::endl;
+    std::cout << "hierarchical parallelism wrong reduction value check" << sum << " correct value " << sum_host
+              << std::endl;
 }
 
 template<typename T>
@@ -70,7 +71,9 @@ void test_complex()
 
 int main()
 {
+  std::cout << "Testing float" << std::endl;
   test_complex<float>();
+  std::cout << "Testing double" << std::endl;
   test_complex<double>();
   return 0;
 }
