3369e4f05119a08b9e56164321b9d651247f03f3
Ye Luo <yeluo@anl.gov>
2020-10-08 15:28:30 -0500

    Add a missing taskwait.

--- hands-on/tests/target_task/target_nowait_task.cpp
+++ hands-on/tests/target_task/target_nowait_task.cpp
@@ -2,17 +2,20 @@
 
 int main()
 {
- int a = 0;
- #pragma omp target map(tofrom: a) depend(out: a) nowait
- {
-   int sum = 0;
-   for (int i = 0; i < 100000; i++)
-     sum++;
-   a = 1;
- }
+  int a = 0;
+  #pragma omp target map(tofrom: a) depend(out: a) nowait
+  {
+    int sum = 0;
+    for (int i = 0; i < 100000; i++)
+      sum++;
+    a = 1;
+  }
 
- #pragma omp task depend(in: a) shared(a)
- {
-   assert(a == 1);
- }
+  #pragma omp task depend(in: a) shared(a)
+  {
+    assert(a == 1);
+  }
+
+  #pragma omp taskwait
+  return 0;
 }
