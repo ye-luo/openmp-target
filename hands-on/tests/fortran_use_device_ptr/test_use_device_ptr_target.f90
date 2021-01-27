4d03470af9e6c7222dd25ae35508cf709c274797
Ye Luo <yeluo@anl.gov>
2021-01-27 11:54:53 -0600

    Add test_use_device_ptr_target.

---
+++ hands-on/tests/fortran_use_device_ptr/test_use_device_ptr_target.f90
@@ -0,0 +1,19 @@
+program test_use_device_ptr
+  implicit none
+  double precision :: alpha
+  integer, parameter :: lda = 10
+  double precision, allocatable :: mat(:, :)
+
+  allocate(mat(lda, lda))
+  call dgemm(lda, mat)
+
+  contains
+    subroutine dgemm(lda, a)
+      implicit none
+      integer :: lda
+      double precision, target:: a(lda,*) ! need target attribute to use c_loc
+      !$omp target data use_device_ptr(a)
+        !call cublas_dgemm('T','N',M,N,K,alpha,c_loc(A),LDA,c_loc(b) +,LDB,beta,c_loc(c),LDC)
+      !$omp end target data
+    end subroutine
+end program
