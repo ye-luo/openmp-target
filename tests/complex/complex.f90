61f5c7613d20cd44688b1d89927c830d91502f04
Ye Luo <yeluo@anl.gov>
2021-04-17 23:12:42 -0500

    Add fail return

--- tests/complex/complex.f90
+++ tests/complex/complex.f90
@@ -1,11 +1,15 @@
 program test_complex
+    implicit none
     complex :: a, b, c
+
     a = cmplx(0, 1)
     b = cmplx(0.5, 0.3)
-    !$omp target map(from, c)
+    !$omp target map(from: c)
     c = a*b
     !$omp end target
+
     if (abs(c - a*b) > 1e-7) then
       print *, "wrong value ", c, "right value ", a*b
+      stop 1
     endif
 end program test_complex
