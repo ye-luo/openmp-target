c9f3e191c99776e15ee88ed5c939354c88dfb2da
Ye Luo <yeluo@anl.gov>
2021-05-18 19:38:27 -0500

    Add host_bug_libomp

---
+++ tests/omphost/README.md
@@ -0,0 +1,22 @@
+Bug report
+https://bugs.llvm.org/show_bug.cgi?id=42393
+
+$ icpx -fiopenmp -fopenmp-targets=spir64 debug.cpp
+
+$ OMP_NUM_THREADS=2 OMP_TARGET_OFFLOAD=MANDATORY ./a.out
+tid = 1
+  0  1  2  3
+tid = 0
+  0  1  2  3
+
+$ OMP_NUM_THREADS=2 OMP_TARGET_OFFLOAD=DISABLED ./a.out
+tid = 0
+  0  1  0  0
+tid = 1
+  0  0  2  3
+
+$ icpx -fiopenmp debug.cpp
+tid = 0
+  0  1  0  0
+tid = 1
+  0  0  2  3
---
+++ tests/omphost/host_bug_libomp.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#ifdef _OPENMP
+#include <omp.h>
+#else
+int omp_get_thread_num() { return 1; }
+#endif
+
+int main()
+{
+  const int size = 4;
+  int wrong_counts = 0;
+  #pragma omp parallel reduction(+:wrong_counts)
+  {
+    int A[size];
+    for(int i = 0; i < size; i++)
+      A[i] = 0;
+
+    #pragma omp target teams distribute map(tofrom: A[:size])
+    for(int i = 0; i < size; i++)
+    {
+      A[i] = i;
+    }
+
+    #pragma omp critical
+    {
+      std::cout << "tid = " << omp_get_thread_num() << std::endl;
+      for(int i = 0; i < size; i++)
+      {
+        if (A[i] != i) wrong_counts++;
+        std::cout << "  " << A[i];
+      }
+      std::cout << std::endl;
+    }
+  }
+  return wrong_counts;
+}
