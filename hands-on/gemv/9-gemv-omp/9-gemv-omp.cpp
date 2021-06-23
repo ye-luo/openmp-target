11e54354742e351f09f3d635faab31db2fd6d535
maletich <86257358+maletich@users.noreply.github.com>
2021-06-23 10:00:19 -0500

    Update 9-gemv-omp.cpp

--- hands-on/gemv/9-gemv-omp/9-gemv-omp.cpp
+++ hands-on/gemv/9-gemv-omp/9-gemv-omp.cpp
@@ -8,7 +8,7 @@ void gemv(int n, T alpha, const T* __restrict__ A, const T* __restrict__ V, T* _
 {
 //#pragma omp parallel for // No parallel for vanilla code
   for (int row = 0; row < n; row++)
-  { // Multiplies input matrix A with vector V and returns resulting matrix in Vout
+  { // Multiplies input matrix A with vector V and returns resulting vector in Vout
     T sum                       = T(0); // Initialize sum
     const T* __restrict__ A_row = A + row * n;
     for (int col = 0; col < n; col++) // 
@@ -19,23 +19,24 @@ void gemv(int n, T alpha, const T* __restrict__ A, const T* __restrict__ V, T* _
 
 template<class T>
 T* allocate(size_t n)
-{ // Creates matrix
-  T* ptr = new T[n];
-  std::fill_n(ptr, n, T(1));
-  return ptr;
+{ // Creates matrix or vector
+  T* ptr = new T[n]; // Creates a pointer to matrix of type T
+  std::fill_n(ptr, n, T(1)); // ptr is the pointer to the matrix, _n is size of vector, 
+  //n in the parameters is how many values from start value to fill, T(1) is the value
+  return ptr; // returns pointer to matrix
 }
 
 template<class T>
 void deallocate(T* ptr, size_t n)
-{
+{ // Frees up matrix data
   delete[] ptr;
 }
 
 int main()
 {
-  auto* A    = allocate<float>(N * N);
-  auto* V    = allocate<float>(N);
-  auto* Vout = allocate<float>(N);
+  auto* A    = allocate<float>(N * N); // Creates matrix A
+  auto* V    = allocate<float>(N * N); // Created vector V, now creates matrix V
+  auto* Vout = allocate<float>(N * N); // Created vector Vout, now creates matrix Vout
 
   {
     Timer local("GEMV");
@@ -50,6 +51,6 @@ int main()
     }
 
   deallocate(A, N * N);
-  deallocate(V, N);
-  deallocate(Vout, N);
+  deallocate(V, N * N);
+  deallocate(Vout, N * N);
 }
