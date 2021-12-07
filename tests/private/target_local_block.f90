f042e9d255b521e07ca32fe08799e736d4b1c70c
Ye Luo <yeluo@anl.gov>
2021-12-07 16:56:56 -0600

    Add target_local_block.

---
+++ tests/private/target_local_block.f90
@@ -0,0 +1,27 @@
+program main
+  implicit none
+  integer :: a = 10
+  integer :: collect_a = 0
+  integer, parameter :: n = 1024
+  integer, external :: omp_get_team_num
+
+  !$omp target teams map(tofrom: a, collect_a)
+  block
+  integer :: a = 20
+  if (omp_get_team_num() == 0) collect_a = a
+  end block
+  !$omp end target teams
+
+  if (collect_a == 20) then
+     print *,"Success!"
+  else
+     stop 1
+  endif
+
+  if (a == 10) then
+     print *,"Success!"
+  else
+     stop 1
+  endif
+
+end program main
