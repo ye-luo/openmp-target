01ff8b20cf6bdde404846fe0a2b9aab92c2a221d
Ye Luo <yeluo@anl.gov>
2022-02-08 12:48:54 -0600

    Add two_identical_templates

---
+++ tests/linking/two_identical_templates/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <complex>
+
+void test_a();
+void test_b();
+
+int main()
+{
+  test_a();
+  test_b();
+}
---
+++ tests/linking/two_identical_templates/test_b.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <complex>
+
+template<typename T>
+void test_map()
+{
+  std::cout << "map(complex<>)" << std::endl;
+  std::complex<T> a(0.2, 1), a_check;
+#pragma omp target map(from : a_check)
+  {
+    a_check = a;
+  }
+}
+
+void test_b()
+{
+  test_map<float>();
+}
