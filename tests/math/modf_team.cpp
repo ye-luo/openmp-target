eb39634906c05741c2804f7bb1163f24d4078211
Ye Luo <yeluo@anl.gov>
2022-05-09 13:41:55 -0500

    Add pass print out in modf_team.cpp

--- tests/math/modf_team.cpp
+++ tests/math/modf_team.cpp
@@ -15,10 +15,10 @@ inline void getSplineBound(T x, TRESIDUAL& dx, int& ind, int nmax)
   {
 #if defined(USE_FLOOR)
     T ipart = std::floor(x);
-    dx  = x - ipart;
+    dx      = x - ipart;
 #else
     T ipart;
-    dx  = std::modf(x, &ipart);
+    dx = std::modf(x, &ipart);
 #endif
     ind = static_cast<int>(ipart);
     // upper bound
@@ -33,35 +33,37 @@ inline void getSplineBound(T x, TRESIDUAL& dx, int& ind, int nmax)
 int main()
 {
   using T = float;
-  T x = 1.25;
-  T dx = 0;
+  T x     = 1.25;
+  T dx    = 0;
   int ind = 0;
-  #pragma omp target map(from: dx, ind)
+#pragma omp target map(from : dx, ind)
   {
     getSplineBound(x, dx, ind, 10);
   }
 
-  if(x != T(dx + ind))
+  if (x != T(dx + ind))
   {
     std::cout << "Error x = " << x << " dx = " << dx << " ind " << ind << std::endl;
     return 1;
   }
+  std::cout << "omp target passed!" << std::endl;
 
   constexpr int N = 100;
   T x_arr[N];
   T dx_arr[N];
   int ind_arr[N];
   for (int i = 0; i < N; i++)
-    x_arr[i] = (i+1)*0.25;
+    x_arr[i] = (i + 1) * 0.25;
 
-  #pragma omp target teams distribute map(to: x_arr[:N]) map(from: dx_arr[:N], ind_arr[:N])
+#pragma omp target teams distribute map(to : x_arr[:N]) map(from : dx_arr[:N], ind_arr[:N])
   for (int i = 0; i < N; i++)
     getSplineBound(x_arr[i], dx_arr[i], ind_arr[i], 24);
 
-  for (int i = 0; i < N-1; i++)
-    if(x_arr[i] != T(dx_arr[i] + ind_arr[i]))
+  for (int i = 0; i < N - 1; i++)
+    if (x_arr[i] != T(dx_arr[i] + ind_arr[i]))
     {
-      std::cout << "Error team = " << i << " x = " << x_arr[i] << " dx = " << dx_arr[i] << " ind " << ind_arr[i] << std::endl; 
+      std::cout << "Error team = " << i << " x = " << x_arr[i] << " dx = " << dx_arr[i] << " ind " << ind_arr[i]
+                << std::endl;
       return 1;
     }
 
@@ -70,8 +72,10 @@ int main()
     const int i = N - 1;
     if (ind_arr[i] != 24)
     {
-      std::cout << "Error team = " << i << " x = " << x_arr[i] << " dx = " << dx_arr[i] << " ind " << ind_arr[i] << std::endl; 
+      std::cout << "Error team = " << i << " x = " << x_arr[i] << " dx = " << dx_arr[i] << " ind " << ind_arr[i]
+                << std::endl;
       return 1;
     }
   }
+  std::cout << "omp target teams distribute passed!" << std::endl;
 }
