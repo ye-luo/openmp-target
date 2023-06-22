bf1e70e7cbb434c66e181e65d56980e776137009
Ye Luo <yeluo@anl.gov>
2023-06-22 09:58:21 -0500

    More private marking.

--- tests/fortran_allocator/dualspace.f90
+++ tests/fortran_allocator/dualspace.f90
@@ -26,6 +26,9 @@ module dualspace
     procedure :: data => data_cplx_double
   end type
 
+  private :: resize_base, allocate_data, deallocate_data
+  private :: resize_double, resize_cplx_double
+  private :: data_double, data_cplx_double
 contains
   subroutine resize_base(self, bytes)
     implicit none
@@ -93,26 +96,3 @@ contains
     call C_F_POINTER(self%data_ptr, res, shape=[self%dims])
   end function
 end module
-
-subroutine fill_density(density)
-  use dualspace, only: dualspace_double_type, dualspace_cplx_double_type
-  implicit none
-  type(dualspace_double_type), intent(inout) :: density
-  real(8), dimension(:), pointer :: density_data
-  integer, parameter :: test_size =20
-  integer :: i
-
-  call density%resize(test_size)
-  density_data => density%data()
-  write(*,*) "density_data size ", size(density_data)
-  do i = 1, test_size
-    density_data(i) = 1.
-  enddo
-  !$omp target update to(density_data)
-  !$omp target teams distribute parallel do map(always, from: density_data)
-  do i = 1, test_size
-    density_data(i) = density_data(i) + i * 1.
-  enddo
-
-  if (density_data(3).ne.4.) stop 1
-end subroutine
--- tests/fortran_allocator/dualspace_array_resize.f90
+++ tests/fortran_allocator/dualspace_array_resize.f90
@@ -1,3 +1,26 @@
+subroutine fill_density(density)
+  use dualspace, only: dualspace_double_type, dualspace_cplx_double_type
+  implicit none
+  type(dualspace_double_type), intent(inout) :: density
+  real(8), dimension(:), pointer :: density_data
+  integer, parameter :: test_size =20
+  integer :: i
+
+  call density%resize(test_size)
+  density_data => density%data()
+  write(*,*) "density_data size ", size(density_data)
+  do i = 1, test_size
+    density_data(i) = 1.
+  enddo
+  !$omp target update to(density_data)
+  !$omp target teams distribute parallel do map(always, from: density_data)
+  do i = 1, test_size
+    density_data(i) = density_data(i) + i * 1.
+  enddo
+
+  if (density_data(3).ne.4.) stop 1
+end subroutine
+
 program abc
   use dualspace, only: dualspace_double_type, dualspace_cplx_double_type
   implicit none
